45f3c023aa42e8ae078084f4481b9f3fa83dba8d
Ben Altman <altmanguitar@gmail.com>
2020-11-16 18:00:40 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,3 +1,4 @@
+=begin
 def prime?(n)
   range = (2..n/2).to_a
   if n <= 1
@@ -9,24 +10,15 @@ def prime?(n)
     return true
   end
 end
-
-=begin
-prime number is a number that have 0 remainder only if it divisible by 1 or itself,
- easiest way to check if it's prime(not very efficient working with great numbers)
-  it's to try to divide it by every number starting from 2 up till the given number
-   and check if it has remainder of 0 - if it does - it's not a prime.
-   you can check remainder with modulo operator %, google if you need to know how to use it.
-    also prime cannot be negative 0 or 1, so you can set up your edge cases accordingly.
 =end
 
-=begin
 def prime?(n)
-   if n <= 1
-    false
-   elsif (2..n-1).none? {|division| n % division == 0}
-    true
-   else
-    false
-   end
+if n <= 1
+   return false
+ elsif n <= 3
+   return true
+ else (2..n/2).none? do |x|
+   n % x == 0
+ end
+ end
 end
-=end

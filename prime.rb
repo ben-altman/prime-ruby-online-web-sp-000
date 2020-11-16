ad965fbee8889813f0e8c53cca63c8e7b964a494
Ben Altman <altmanguitar@gmail.com>
2020-11-16 18:17:36 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -7,14 +7,3 @@ def prime?(n)
     range.none? {|i| n % i == 0}
   end
 end
-
-=begin
-def prime?(n)
-if n <= 1
-   return false
-# elsif n <= 3
-#   return true
- else (2..n/2).none? {|x| n % x == 0}
- end
-end
-=end

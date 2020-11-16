3fe273ef5c25f7517df73481ff9b6a26f8b697c0
Ben Altman <altmanguitar@gmail.com>
2020-11-16 18:24:27 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,21 +2,22 @@ def prime?(n)
   range = (2..n/2).to_a
   if n <= 1
     return false
-  elsif
-    range.any? {|i| n % i == 0}
-    return false
   else
-    return true
+    range.none? {|i| n % i == 0}
   end
 end
+
 
 =begin
 def prime?(n)
   range = (2..n/2).to_a
   if n <= 1
     return false
+  elsif
+    range.any? {|i| n % i == 0}
+    return false
   else
-    range.none? {|i| n % i == 0}
+    return true
   end
 end
 =end
